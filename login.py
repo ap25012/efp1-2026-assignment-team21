0df3a436206f63dc84a60b9542347e097f1bfb00
VassilisGaroufalis <ap25009@hua.gr>
2026-02-08 21:31:39 +0200

    login

--- login.py
+++ login.py
@@ -13,10 +13,19 @@ PROVIDERS = {
 
 def login1() -> tuple:
     
-    # Θα επιστρέφει True ή False τον ρόλο και το Username
-    # Role: "user" ή "provider" ή "None"
+# Θα επιστρέφει True ή False τον ρόλο και το Username
+# Role: "user" ή "provider" ή "None"
     
     print("\n--- Σύστημα Εισόδου ---")
     role_input = input("Επιλέξτε ρόλο (1 για User, 2 για Provider): ")
     username = input("Username: ")
     password = input("Password: ")
+
+    if role_input == "1":
+        # Έλεγχος στη λίστα USERS
+        if username in USERS and USERS[username] == password:
+            print(f"Επιτυχής σύνδεση ως Χρήστης ({username}).\n")
+            return True, "user", username
+        else:
+            print("Λάθος στοιχεία χρήστη.\n")
+            return False, None, None
