8e97f13ae30920e632e8ce68cb89d8d6531d9ebe
VassilisGaroufalis <ap25009@hua.gr>
2026-02-08 21:02:07 +0200

    login

---
+++ login.py
@@ -0,0 +1,12 @@
+# login.py
+# Λίστα εγκεκριμένων χρηστών (Δωρητές)
+USERS = {
+    "user": "1234",
+    "User": "123",
+}
+
+# Λίστα εγκεκριμένων παρόχων (Providers)
+PROVIDERS = {
+    "provider": "abcd",
+    "Provider": "ABC",
+}
