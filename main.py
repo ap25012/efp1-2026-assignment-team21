296ee5a4add76515a0fdb24b4646fbb7279b3955
kostas <ap25012@hua.gr>
2026-02-19 12:19:58 +0200

    main2

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def main():
 
     if not is_success:
         return # Exit if authentication fails
+    
+     # --- SYSTEM INITIALIZATION & IDENTIFICATION ---
+    system = DonationSystem()
+    print(f" ΚΑΛΩΣΗΡΘΑΤΕ ({username}) - ΑΠΑΙΤΕΙΤΑΙ ΤΑΥΤΟΠΟΙΗΣΗ")
+    print("Παρακαλώ συμπληρώστε τα στοιχεία σας για να εισέλθετε:")
+
+    try:
+        # Collecting real-world identity details to create the session
+        full_name = input("Ονοματεπώνυμο: ")
+        phone = int(input("Τηλέφωνο: "))
+        email = input("Email: ")
+        afm = int(input("ΑΦΜ: "))
+    except ValueError:
+        print("Σφάλμα: Το τηλέφωνο και το ΑΦΜ πρέπει να είναι αριθμοί.")
+        return
+
+    current_user = None
+    current_provider = None
